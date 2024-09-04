61798f6beef51c74dce376c337ef18426ab54c42
studern-bzz <studern@bzz.ch>
2024-09-04 14:45:04 +0200

    Caesar done

--- caesar.py
+++ caesar.py
@@ -1,6 +1,7 @@
 alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
 def encode(text, key):
+    key = int(key)
     key = key % 26
     coded_text = ""
     text = text.upper()
@@ -16,6 +17,7 @@ def encode(text, key):
     return coded_text
 
 def decode(text, key):
+    key = int(key)
     key = key % 26
     decoded_text = ""
     text = text.upper()
@@ -33,6 +35,16 @@ def decode(text, key):
 
 
 if __name__ == '__main__':
-    print(encode("Some example Text", 11112))
-    print(decode("CYWOOHKWZVODOHD",11112))
+    print(encode("abc", 3))
+    print(decode("def", 3))
+    decision = input("Do you want to code(c) or decode(d)?")
+    decision = decision.upper()
+    if decision == "C":
+        phrase = input("What string do you want to Code?")
+        key = input("Whats the key you want to use?")
+        print(f"Your coded phrase is: {encode(phrase,key)}")
+    if decision == "D":
+        phrase = input("What string do you want to decode?")
+        key = input("Whats the key you want to use?")
+        print(f"Your decoded phrase is: {decode(phrase,key)}")
 
---
+++ vigenere.py
@@ -0,0 +1,12 @@
+alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+def code(text, key):
+    coded_text = ""
+    text = text.upper()
+    text_list = list(text)
+
+
+
+
+
+if __name__ == '__main__':
